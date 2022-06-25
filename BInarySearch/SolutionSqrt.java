e45c1f74a4fb3c347b9028123631208a62efc828
19ahb <19ahb@queensu.ca>
2022-06-25 13:12:30 -0400

    Sqrt(x) Solution

---
+++ BInarySearch/SolutionSqrt.java
@@ -0,0 +1,27 @@
+package BInarySearch;
+
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        if(x == 1) return 1;
+        
+        int left = 1;
+        int right = x;
+        
+        while(left <= right){
+            int middle = left + ((right-left)/2);
+            
+            if(middle == x / middle) 
+                return middle;
+            else if(middle > x / middle) 
+                right = middle - 1;
+            else if (middle < x / middle) 
+                left = middle + 1;
+            
+            
+        }//close while
+        
+        return right;
+    }
+       
+}
