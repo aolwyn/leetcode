cd7d4e423c4d5b45598bc6c22853ee96af447298
Adam Bayley <94658867+aolwyn@users.noreply.github.com>
2022-06-27 11:50:10 -0400

    Update count odd numbers in an interval.java

--- Math/count odd numbers in an interval.java
+++ Math/count odd numbers in an interval.java
@@ -6,6 +6,13 @@ class Solution {
         
         return oddNumbers;
         
-        
+        /*
+            int count = 0;
+        for(int i = low; i <= high; i++){
+            if(i%2!=0) count++;
+        }
+        return count;
+      The above also works but takes up a lot of time.
+        */
     }
 }
