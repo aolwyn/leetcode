624d8bfc90285fff1050c039add9230d054f5c9b
Adam Bayley <94658867+aolwyn@users.noreply.github.com>
2022-06-27 15:35:35 -0400

    Update Isomorphic Strings.java

--- Strings/Isomorphic Strings.java
+++ Strings/Isomorphic Strings.java
@@ -4,11 +4,11 @@ class Solution {
         if( s == null || t == null || s.length() != t.length()) return false; 
         
       //two hashmaps for the chars
-        Map<Character, Integer> charToIndex_s = new HashMap<>();
-        Map<Character, Integer> charToIndex_t = new HashMap<>();
+        Map<Character, Integer> charT = new HashMap<>();
+        Map<Character, Integer> charS = new HashMap<>();
 
         for (Integer i = 0; i < s.length(); i++)
-            if (charToIndex_s.put(s.charAt(i), i) != charToIndex_t.put(t.charAt(i), i))
+            if (charT.put(s.charAt(i), i) != charS.put(t.charAt(i), i))
                 return false;
 
         return true;
