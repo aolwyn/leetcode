0aa37fcf908c8dcbf092f84c08695dac04beff91
Adam Bayley <94658867+aolwyn@users.noreply.github.com>
2022-06-29 10:58:11 -0400

    Create sign of the product of an array.java

---
+++ Arrays/sign of the product of an array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        double product = 1;
+        
+        for(int i = 0; i < nums.length; i++){
+            product = product * nums[i];
+        }//close for
+            if(product < 0) return -1;
+        else if(product > 0) return 1;
+        else return 0;
+    }
+}
